d97c9abd10b684f53950a25fa86f849cbcefae8b
Shahanaj2000 <shahanajmt@gmail.com>
2023-01-20 20:29:41 +0530

    CLIPRECT

---
+++ lib/Lecture/ClipWidgets/ClipRect.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: CLIPRECT(),
+  ));
+}
+
+class CLIPRECT extends StatelessWidget {
+  const CLIPRECT({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CLIPRECT"),
+      ),
+      body: ListView(
+        children: [
+          ClipRect(
+            clipBehavior: Clip.hardEdge,
+            child: Container(
+              child:  Align(
+                alignment: Alignment.center,
+                widthFactor: 150,
+                heightFactor: 100,
+                child: Image.network( 'https://images.unsplash.com/photo-1473992243898-fa7525e652a5'),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10,)
+        ], 
+      ),
+    );
+  }
+}
